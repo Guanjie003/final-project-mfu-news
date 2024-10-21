3dd04a8ea81a22df169933498753c3d506ddc2c3
WANTANA SUWANNAPHO <156871926+wrtnn@users.noreply.github.com>
2024-10-21 15:26:14 +0000

    Add react js

---
+++ web_frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Common/Header'
+
+function App() {
+  return (
+    <>
+      //Code here
+    </>
+  );
+}
+
+export default App;
